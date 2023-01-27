687c8043923eb34a202012b5619585000ada1388
Steven Hill <steven.hill@holidayextras.com>
2023-01-27 12:14:01 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,7 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-source-prismic`,
-      options: {
-        repositoryName: `my-first-gatsby-site-main`,
-        accessToken: `${process.env.API_KEY}`,
-        linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
-        schemas: {
-          post: require("./custom_types/post.json"),
-        },
-      },
-    },
-  ],
-}require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+  siteMetadata: {
+    title: `My First Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
