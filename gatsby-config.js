770535419964b3b77dd6e151ae800d142afd2325
Steven Hill <steven.hill@holidayextras.com>
2023-01-27 13:31:28 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       resolve: `gatsby-source-prismic`,
       options: {
-        repositoryName: `your-repository-name`,
+        repositoryName: `airparksde`,
         accessToken: `${process.env.API_KEY}`,
         linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
         schemas: {
