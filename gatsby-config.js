e612e9e33b3170bfef9980d4bfb4b31ba5841d11
Steven Hill <steven.hill@holidayextras.com>
2023-01-27 12:53:39 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,1 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
 
-module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-source-prismic`,
-      options: {
-        repositoryName: `myfirstgatsbysitemain97955`,
-        accessToken: `${process.env.API_KEY}`,
-        linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
-        schemas: {
-          post: require("./custom_types/post.json"),
-        },
-      },
-    },
-  ],
-}
