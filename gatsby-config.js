88d2f354bded26bd280d5c5db3b6a12b06cfa2b1
Steven Hill <steven.hill@holidayextras.com>
2023-01-27 12:10:47 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ module.exports = {
     {
       resolve: `gatsby-source-prismic`,
       options: {
-        repositoryName: `myfirstgatsbysitemain97955`,
-        accessToken: `${process.env.MC5ZOU9lSWhJQUFDSUFhVTBi.77-9WHMMEO-_ve-_ve-_vTXvv73vv73vv73vv73vv73vv73vv71_77-977-9Ne-_ve-_vTZP77-9Iu-_vXfvv73vv73vv73vv70}`,
+        repositoryName: `airparks-de`,
+        accessToken: `${process.env.API_KEY}`,
         linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
         schemas: {
           post: require("./custom_types/post.json"),
@@ -19,19 +19,3 @@ module.exports = {
 }require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
-
-module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-source-prismic`,
-      options: {
-        repositoryName: `your-repository-name`,
-        accessToken: `${process.env.API_KEY}`,
-        linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
-        schemas: {
-          post: require("./custom_types/post.json"),
-        },
-      },
-    },
-  ],
-}
