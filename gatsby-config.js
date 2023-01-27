3c5c64f7a3b4503d3cb849b9ca3e931196b18be9
Steven Hill <steven.hill@holidayextras.com>
2023-01-27 11:11:57 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,19 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
-require('dotenv').config({
+require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-const routes = [
-  {
-    type: "article",
-    path: "/articles/:uid",
-  },
-  {
-    type: "page",
-    path: "/:uid",
-  },
-];
-
 module.exports = {
   plugins: [
-    // ...
-    'gatsby-plugin-image',
     {
-      resolve: 'gatsby-source-prismic',
+      resolve: `gatsby-source-prismic`,
       options: {
-        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
-        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
-        routes,
+        repositoryName: `your-repository-name`,
+        accessToken: `${process.env.API_KEY}`,
+        linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
+        schemas: {
+          post: require("./custom_types/post.json"),
+        },
       },
     },
-  ] 
+  ],
 }
