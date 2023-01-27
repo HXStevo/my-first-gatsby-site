abaf655ca73fad71fe81638b731d3f2dbc8e74c4
Steven Hill <steven.hill@holidayextras.com>
2023-01-27 11:37:40 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,42 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
 
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
 module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  },
   plugins: [
+    `gatsby-plugin-image`,
     {
-      resolve: `gatsby-source-prismic`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        repositoryName: `my-first-gatsby-site-main`,
-        accessToken: `${process.env.API_KEY}`,
-        linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
-        schemas: {
-          post: require("./custom_types/post.json"),
-        },
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
